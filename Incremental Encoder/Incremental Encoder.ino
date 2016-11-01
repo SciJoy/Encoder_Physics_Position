d9169252faafd5c98304b2387ddd7637b098c341
SciJoy <contactscijoy@gmail.com>
2016-11-01 14:34:48 -0400

    update video info

--- Incremental Encoder/Incremental Encoder.ino
+++ Incremental Encoder/Incremental Encoder.ino
@@ -1,8 +1,8 @@
 /*  
 Building an Incremental Encoder
 http://youtu.be/j8rG3bjqriI - Encoder Basics
-http://youtu.be/ -How to Build and Encoder
-http://youtu.be/ -How to Code the Encoder
+https://youtu.be/LjY1PSd8w2E -How to Build and Encoder
+https://youtu.be/CM0GePGEtrs -How to Program the Encoder
 
 This code is for an incremental encoder. It reads the state of a break beam, 
 compares it to the previous state, and increments the number of state changes
